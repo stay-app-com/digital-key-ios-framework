f0d205fb8caaecb0f9f3e01b91d300b5986eec59
Antonio Oliva <157590202+Antonio-Stay@users.noreply.github.com>
2024-02-13 16:24:58 +0100

    Add files via upload

---
+++ StayDigitalKeyFramework.xcframework/ios-arm64_x86_64-simulator/StayDigitalKeyFramework.framework/Headers/DigitalKeyProviderInputProtocol.h
@@ -0,0 +1,34 @@
+//
+//  DigitalKeyProviderInputProtocol.h
+//  StayWeex
+//
+//  Created by Alberto Moraga on 28/9/18.
+//  Copyright © 2018 taobao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DigitalKeyProviderInputProtocol <NSObject>
+
+- (void) startProcess;
+
+- (BOOL)isUserRegistered;
+- (BOOL)needsMobilePhoneRegistration;
+- (BOOL)needsEmailRegistration;
+- (BOOL)needsSMSConfirmation;
+- (BOOL)needsBlockButtonDesactive;
+- (BOOL)hasMobilePhoneStored;
+- (void)reloadKeys;
+- (void)activateKeyWithIndex:(NSInteger)indexKey withError:(NSError **)error;
+- (void)deactivateKeyWithIndex:(NSInteger)indexKey withError:(NSError **)error;
+
+@optional
+- (void)setPhoneNumber:(NSString *)number;
+- (NSString *)storedMobilePhone;
+- (void)setEmail:(NSString *)email;
+- (void)setInvitationCode:(NSString *)code;
+- (void)setSMSCode:(NSString *)smsCode;
+- (void)resendSMSCode;
+
+
+@end
